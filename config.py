77a68142230ac3390c38e714c0b3b6c8845c856a
xaiko <magnumx019@gmail.com>
2024-07-19 03:17:47 +0700

    Update config.py
    Signed-off-by: xaiko <magnumx019@gmail.com>

--- config.py
+++ config.py
@@ -14,15 +14,15 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://ohmyqueenmedusa:ohmyqueenmedusa@ohmyqueenmedusa.vnxmi0m.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1200))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", "-1002020845157"))
+LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 1318826936))
+OWNER_ID = int(getenv("OWNER_ID", 6474577612))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
